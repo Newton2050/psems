e48cda0c6b0716f36e023d3f76943523306d1f1a
NEWTON KAIRU <42859022+Newton2050@users.noreply.github.com>
2026-07-31 18:18:32 +0300

    Add Stream model metadata: PHPDoc, fillable, and relationships

--- app/Models/Stream.php
+++ app/Models/Stream.php
@@ -1,1 +1,31 @@
+<?php
+namespace App\Models;
 
+use App\Core\Model;
+
+/**
+ * Class Stream
+ *
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property \DateTimeInterface|null $created_at
+ * @property \DateTimeInterface|null $updated_at
+ */
+class Stream extends Model
+{
+    protected string $table = 'streams';
+
+    protected array $fillable = [
+        'name',
+        'description',
+    ];
+
+    // Example: a stream has many pupils
+    public function pupils()
+    {
+        return $this->hasMany(Pupil::class, 'stream_id');
+    }
+}
