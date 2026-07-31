b4724b6f7469ae89501383bbd9f8f60e0e1edf5c
NEWTON KAIRU <42859022+Newton2050@users.noreply.github.com>
2026-07-31 18:19:36 +0300

    Add Teacher model metadata: PHPDoc, fillable, and relationships

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -1,1 +1,34 @@
+<?php
+namespace App\Models;
 
+use App\Core\Model;
+
+/**
+ * Class Teacher
+ *
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $first_name
+ * @property string|null $middle_name
+ * @property string $last_name
+ * @property string|null $email
+ * @property \DateTimeInterface|null $created_at
+ * @property \DateTimeInterface|null $updated_at
+ */
+class Teacher extends Model
+{
+    protected string $table = 'teachers';
+
+    protected array $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name',
+        'email',
+    ];
+
+    public function subjects()
+    {
+        return $this->hasMany(Subject::class, 'teacher_id');
+    }
+}
