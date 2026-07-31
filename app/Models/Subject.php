42cde5afb4eb81db33431f24ef5e67461ea9091d
NEWTON KAIRU <42859022+Newton2050@users.noreply.github.com>
2026-07-31 18:19:05 +0300

    Add Subject model metadata: PHPDoc, fillable, and relationships

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -1,1 +1,37 @@
+<?php
+namespace App\Models;
 
+use App\Core\Model;
+
+/**
+ * Class Subject
+ *
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $code
+ * @property int|null $teacher_id
+ * @property \DateTimeInterface|null $created_at
+ * @property \DateTimeInterface|null $updated_at
+ */
+class Subject extends Model
+{
+    protected string $table = 'subjects';
+
+    protected array $fillable = [
+        'name',
+        'code',
+        'teacher_id',
+    ];
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class, 'teacher_id');
+    }
+
+    public function marks()
+    {
+        return $this->hasMany(Mark::class, 'subject_id');
+    }
+}
