8586f771306dad038bec7e5d0a408fbb80696145
NEWTON KAIRU <42859022+Newton2050@users.noreply.github.com>
2026-07-31 18:17:51 +0300

    Add Setting model metadata: PHPDoc and fillable

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -1,1 +1,30 @@
+<?php
+namespace App\Models;
 
+use App\Core\Model;
+
+/**
+ * Class Setting
+ *
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $key
+ * @property string|null $value
+ * @property \DateTimeInterface|null $created_at
+ * @property \DateTimeInterface|null $updated_at
+ */
+class Setting extends Model
+{
+    protected string $table = 'settings';
+
+    protected array $fillable = [
+        'key',
+        'value',
+    ];
+
+    protected array $casts = [
+        // store JSON or other structured settings if needed
+        'value' => 'string',
+    ];
+}
